84b81e035856cefad039b1d32db61e6151ad5512
Karzhas Tokbolatov <tokbolatov.k@gmail.com>
2021-10-23 21:22:29 +0600

    .

--- src/main/java/kz/fizmat/fizmatappbykarzhas/TestController.java
+++ src/main/java/kz/fizmat/fizmatappbykarzhas/TestController.java
@@ -1,7 +1,16 @@
 package kz.fizmat.fizmatappbykarzhas;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
+
+    @GetMapping("/test")
+    public int testRest(){
+        System.out.println("ASDASDASD");
+        return 3;
+    }
+
 }
