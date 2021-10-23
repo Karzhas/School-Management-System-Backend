56572d8347f6eecfcaab23e4b66b8ea40d7982af
Karzhas Tokbolatov <tokbolatov.k@gmail.com>
2021-10-23 18:35:59 +0600

    first commit

---
+++ src/main/java/kz/fizmat/fizmatappbykarzhas/FizmatAppByKarzhasApplication.java
@@ -0,0 +1,13 @@
+package kz.fizmat.fizmatappbykarzhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizmatAppByKarzhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizmatAppByKarzhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/fizmat/fizmatappbykarzhas/FizmatAppByKarzhasApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.fizmat.fizmatappbykarzhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizmatAppByKarzhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
